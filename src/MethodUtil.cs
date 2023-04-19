179450f1a8899f79631609d6b94d57b64b2fe7dc
Jake Soenneker <jake@soenneker.com>
2023-04-19 13:38:20 -0500

    Initial commit

---
+++ src/MethodUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+
+namespace Soenneker.Utils.Method;
+
+/// <summary>
+/// A utility library providing various method() related operations
+/// </summary>
+public static class MethodUtil
+{
+    /// <summary>
+    /// Gets the name of the current method (calling this)
+    /// </summary>
+    /// <param name="methodName">Not intended to ever be set from caller</param>
+    [Pure]
+    public static string? Get([CallerMemberName] string? methodName = null)
+    {
+        return methodName;
+    }
+}
